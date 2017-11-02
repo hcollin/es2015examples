ad42d5d77f5e45eaf4a052996b3f729df5d2a6b7
Henrik Collin <hcollin78@gmail.com>
2017-11-02 18:02:29 +0200

    Arrow function example 1

---
+++ part1/arrayfunctions/arrows.js
@@ -0,0 +1,37 @@
+/**
+ * Example of arrow functions
+ *
+ * Run with node arrows.js
+ *
+ * Henrik Collin <henrik.collin@cinia.fi>
+ * (c) Cinia Solutions Oy 2017
+ */
+
+
+
+function oldWay() {
+    this.a = 0;
+    const clearMe = setInterval(function() {
+        this.a++;
+        console.log("tick ", this.a);
+        if(this.a >=5) {
+            clearInterval(clearMe);
+        }
+    }, 200);
+}
+
+function newWay() {
+    this.a = 0;
+    const clearMe = setInterval( () => {
+        this.a++;
+        console.log("tick ", this.a);
+        if(this.a >=5) {
+            clearInterval(clearMe);
+        }
+    }, 200);
+}
+
+
+
+oldWay();
+//newWay();
